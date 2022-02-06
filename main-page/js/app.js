9562dd859ccda3ad9f6a4ea950cda10875fed05a
yubi-ece <53133157+yubi-ece@users.noreply.github.com>
2022-02-06 16:29:50 -0500

    Create app.js

---
+++ main-page/js/app.js
@@ -0,0 +1,19 @@
+
+
+
+// Back to top arrow button
+
+const backToTopBtn = $("#backToTopBtn");
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 300) {
+    backToTopBtn.addClass("show");
+  } else {
+    backToTopBtn.removeClass("show");
+  }
+});
+
+backToTopBtn.on("click", function (e) {
+  e.preventDefault();
+  $("html, body").animate({ scrollTop: 0 }, "300");
+});
